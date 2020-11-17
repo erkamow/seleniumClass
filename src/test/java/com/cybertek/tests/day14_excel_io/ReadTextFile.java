3dfa277eea0577d90e767daeb3905bf386ab315c
erkamow <erkamafsin@afsteknik.com>
2020-11-17 17:33:08 +0300

    ReadingExcelFile

--- src/test/java/com/cybertek/tests/day14_excel_io/ReadTextFile.java
+++ src/test/java/com/cybertek/tests/day14_excel_io/ReadTextFile.java
@@ -7,10 +7,11 @@ import java.util.Scanner;
 public class ReadTextFile {
 
     public static void main(String[] args) throws FileNotFoundException {
+        //path to file that we want to read
         String path = "shopping_list.txt";
+        //File's constructor requires path to the file as an argument
         File file = new File(path);
-        //check if file exist
-
+        //check if file exists
         System.out.println(file.exists());
         //we can read lines of file with Scanner class
         //System.in - for keyboard input
@@ -22,6 +23,15 @@ public class ReadTextFile {
 
         while (scanner.hasNext()){
             System.out.println(scanner.nextLine());
+        }
+//        ########################################
+
+        String path2 = "grades.csv";
+        File file2 = new File(path2);
+        Scanner scanner2 = new Scanner(file2);
+
+        while (scanner2.hasNext()){
+            System.out.println(scanner2.nextLine());
         }
     }
 }
